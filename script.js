21a9dc6c1b4f3e60104704c4661ed21aa51cc555
ChiefBibek <114321426+ChiefBibek@users.noreply.github.com>
2022-09-27 14:41:55 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-              strings: [ "Myla","vlogger", "Artist", "tattooist", "Designer"],
+        strings: [ "Myla","vlogger", "Artist", "tattooist", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-             strings: [ "Myla","vlogger", "Artist", "tattooist", "Designer"],
+        strings: [ "Myla","vlogger", "Artist", "tattooist", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
